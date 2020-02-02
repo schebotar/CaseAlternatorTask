5c84bc7391ffeb6d6eda3902ab9a62048d3d2d6e
Sergey Chebotar <s.chebotar@gmail.com>
2020-02-02 18:49:09 +0300

    Add project files.

---
+++ CaseAlternatorTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseAlternatorTask
+{
+    public class CaseAlternatorTask
+    {
+        //Вызывать будут этот метод
+        public static List<string> AlternateCharCases(string lowercaseWord)
+        {
+            var result = new List<string>();
+            AlternateCharCases(lowercaseWord.ToCharArray(), 0, result);
+            return result;
+        }
+
+        static void AlternateCharCases(char[] word, int startIndex, List<string> result)
+        {
+            // TODO
+            result.Add(new string(word));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseAlternatorTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine();
+            var result = CaseAlternatorTask.AlternateCharCases(word);
+            foreach (var element in result)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
